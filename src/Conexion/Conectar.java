d237b92d86c5efb2a717242c98d93108c6d18423
angelsanchue <angelsanchue@gmail.com>
2022-02-15 02:40:44 -0600

    Primer Commit

---
+++ src/Conexion/Conectar.java
@@ -0,0 +1,48 @@
+
+
+package Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Ignacio Padilla Huerta
+ */
+public class Conectar {
+
+private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String USER = "root"; 
+    private static final String PASSWORD = "password";
+    private static final String URL = "jdbc:mysql://localhost:3306/inventariosdb";
+    
+    private Connection conn;
+    
+    public Conectar(){
+        conn = null;
+    }
+    
+    public Connection getConnection(){
+        try {
+            Class.forName(DRIVER);
+            conn = DriverManager.getConnection(URL,USER,PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(),"Error al conectar con la base de datos", JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+        }
+        return conn;
+    }
+    
+    public void desconectar(){
+        try {
+            conn.close();
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(),"Error al cerrar la conexion con la base de datos", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+}
+
